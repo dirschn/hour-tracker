405f4b1ca42bd6c1878be660490cc714814ed2f8
Nick Dirschel <nicholas.dirschel@parsons.us>
2025-08-22 19:08:15 -0400

    segregate dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './guards/auth.guard';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./components/dashboard.component').then(m => m.DashboardComponent),
+    loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent),
     canActivate: [AuthGuard]
   },
   {
